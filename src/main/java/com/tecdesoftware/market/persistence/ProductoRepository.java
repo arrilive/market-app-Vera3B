693b6c05bd5a87c1a3474fa512d0583ccc0dd317
arrilive <lv20051309@gmail.com>
2025-07-02 11:28:31 -0600

    MapStruct implemented

--- src/main/java/com/tecdesoftware/market/persistence/ProductoRepository.java
+++ src/main/java/com/tecdesoftware/market/persistence/ProductoRepository.java
@@ -2,9 +2,13 @@ package com.tecdesoftware.market.persistence;
 
 import com.tecdesoftware.market.persistence.crud.ProductoCrudRepository;
 import com.tecdesoftware.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+//Le dice a Spring que este repositorio se conecta con la BD
+@Repository
 public class ProductoRepository {
 
     private ProductoCrudRepository productoCrudRepository;
@@ -13,6 +17,31 @@ public class ProductoRepository {
     public List<Producto> getAll(){
         //Convirtiendo un iterable<t> a una lista de productos List<Producto>
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    //Obtiene los productos por categoria
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIDCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    //Obtiene estado del producto
+    public Optional<List<Producto>> getBEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    //Obtiene un producto dado el id
+    public Optional<Producto> findById(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    //Guarda un prducto
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    //Borrar un prodcuto
+    public void delete(Producto producto){
+        productoCrudRepository.delete(producto);
     }
 }
 
--- src/main/java/com/tecdesoftware/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/tecdesoftware/market/persistence/crud/ProductoCrudRepository.java
@@ -3,5 +3,17 @@ package com.tecdesoftware.market.persistence.crud;
 import com.tecdesoftware.market.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+
+    //Query Methods
+    //SQL: SELECT *
+    //FROM Categoria
+    //WHERE id_categoria = x
+    //ORDER BY Nombre ASC
+    List<Producto> findByIDCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(Integer cantidadStockIsLessThan, Boolean estado);
 }
